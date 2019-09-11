f40e6d7b3a8a71ec29b0684561bd79e8e71c7ae1
Willaim <bluesnakeuser@gmail.com>
2019-09-11 13:25:22 -0600

    Added more scanners for age

--- src/input/controller/Controller.java
+++ src/input/controller/Controller.java
@@ -5,19 +5,28 @@ public class Controller
 {
 	private Scanner textScanner; //Declaration of the Scanner instance
 //Can't use yet
-	
+
 
 	public Controller() // Constructor - It's job is to initialize variables
 	{
 		textScanner = new Scanner(System.in);
+		
 	}
 
 	public void start()
 	{
 		System.out.println("Type in your name!");
 		String name = "";
+		String age = ""; 
 		name = textScanner.nextLine();
 		System.out.println("Hello! " + name + " how are you??");
+		System.out.println("Please type in some words");
+		name = textScanner.next();
+		System.out.println("You typed in: " + name);
+		System.out.println("Type in your age!");
+		
+		 age = textScanner.next();
+		 System.out.println("You are " + age + " years old?");
 	}
-
+	
 }
